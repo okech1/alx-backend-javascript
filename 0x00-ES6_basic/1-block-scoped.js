fc75dfe2bc39b80c9816c10875b18587113464f8
Livingstone <oklivinstone@gmail.com>
2024-08-05 14:54:22 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // Using let ensures these variables are block-scoped
-    let task2 = false; // Using let ensures these variables are block-scoped
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task2 = false;
   }
 
   return [task, task2];
